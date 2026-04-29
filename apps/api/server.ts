15a357174c228bb394c5cb1ff24b64c22936e1e3
Raunak Dubey <raunak18@proton.me>
2026-04-29 23:51:17 +0530

    feat(auth): Implemented Login Route

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -1,10 +1,11 @@
 import app from './src/app.js';
 import { connectDB } from './src/config/database.js';
 import { env } from './src/config/env.js';
+import logger from './src/config/logger.js';
 
 const PORT = env.PORT || 3000;
 await connectDB();
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
--- apps/api/src/module/auth/auth.controller.ts
+++ apps/api/src/module/auth/auth.controller.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from 'express';
 import { asyncHandler } from '../../utils/asyncHandler.js';
 import { authService } from './auth.service.js';
-import { RegisterUserInput, registerUserSchema } from '@repo/zod-config';
+import {
+  LoginUserInput,
+  loginUserSchema,
+  RegisterUserInput,
+  registerUserSchema,
+} from '@repo/zod-config';
 import { env } from '../../config/env.js';
 
+// ------ Register user -----------------------
 export const registerUser = asyncHandler(
   async (req: Request, res: Response) => {
     const data: RegisterUserInput = await registerUserSchema.parseAsync(
@@ -30,3 +36,25 @@ export const registerUser = asyncHandler(
     });
   },
 );
+
+// ------ Login user -----------------------
+export const loginUser = asyncHandler(async (req: Request, res: Response) => {
+  const data: LoginUserInput = await loginUserSchema.parseAsync(req.body);
+
+  const { accessToken, refreshToken } = await authService.loginUser(data);
+
+  res.cookie('refreshToken', refreshToken, {
+    httpOnly: true,
+    secure: env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    path: '/',
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {
+      accessToken,
+    },
+  });
+});
