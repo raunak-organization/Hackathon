625b2d8c308799ff0a984f4f5b98c9eaea10b0a6
Aushah Gowhar <waniaushahgowhar@gmail.com>
2026-04-30 00:46:06 +0530

    feat(auth): Add auth state and hooks

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
+import { Provider } from 'react-redux';
+import { store } from '../store/app.store';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -24,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Provider store={store}>{children}</Provider>
       </body>
     </html>
   );
